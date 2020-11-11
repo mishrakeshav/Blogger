7d87dd2fb5aab888cdc04ed33292b9669588f9dd
mishrakeshav <keshav.sm@somaiya.edu>
2020-11-11 20:20:49 +0530

    add custom permission

--- Backend/blog_api/views.py
+++ Backend/blog_api/views.py
@@ -3,7 +3,7 @@ from blog.models import Post
 from .serializers import PostSerializer
 from rest_framework.permissions import (
     IsAdminUser , DjangoModelPermissions, 
-    BasePermission,SAFE_METHODS
+    BasePermission,SAFE_METHODS,IsAuthenticatedOrReadOnly
 )
 
 class PostUserWritePermission(BasePermission):
@@ -16,17 +16,14 @@ class PostUserWritePermission(BasePermission):
 
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = [DjangoModelPermissions,]
+    permission_classes = [IsAuthenticatedOrReadOnly,]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView,PostUserWritePermission):
     permission_classes = [PostUserWritePermission,]
     queryset = Post.objects.all() 
     serializer_class = PostSerializer
-
-
-
 
 
 """ Concrete View Classes
