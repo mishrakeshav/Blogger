c478996578cba895336d6660006135410b9b3044
mishrakeshav <keshav.sm@somaiya.edu>
2020-12-18 12:57:03 +0530

    api schema and documentation generation

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -4,6 +4,8 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
@@ -14,4 +16,10 @@ urlpatterns = [
     path('api/', include('blog_api.urls', namespace='blog_api')),
     path('api/user/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('docs/', include_docs_urls(title='Blogger Api')),
+    path('schema/', get_schema_view(
+        title="Blogger",
+        description="Api for Blogger",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
